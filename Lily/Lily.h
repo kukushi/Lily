622779b263f5dab773d9b96c83fee9adbd6c2532
kukushi <17washabi@gmail.com>
2015-03-27 22:15:54 +0800

    Initial Commit

---
+++ Lily/Lily.h
@@ -0,0 +1,19 @@
+//
+//  Lily.h
+//  Lily
+//
+//  Created by kukushi on 3/27/15.
+//  Copyright (c) 2015 kukushi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Lily.
+FOUNDATION_EXPORT double LilyVersionNumber;
+
+//! Project version string for Lily.
+FOUNDATION_EXPORT const unsigned char LilyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lily/PublicHeader.h>
+
+
