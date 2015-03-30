c4e044fd1c7d7f4a5e607c5beb22434c9debe5cb
kukushi <17washabi@gmail.com>
2015-03-30 15:31:47 +0800

    update MemoryCache

--- Lily/Lily.swift
+++ Lily/Lily.swift
@@ -6,8 +6,83 @@
 //  Copyright (c) 2015 kukushi. All rights reserved.
 //
 
-import Cocoa
+import Foundation
 
-class Lily: NSObject {
+let MemoryCache = Lily.shared.memoryCache
+let DiskCache = Lily.shared.diskCache
+let QuickCache = Lily.shared.quickCache
 
+
+private let instance = Lily()
+
+class Lily {
+    class var shared: Lily {
+        return instance
+    }
+    
+    lazy var memoryCache = NSCache()
+    lazy var diskCache = DiskCache()
+    lazy var quickCache = QuickCache()
+    
+    // MARK:
+    
+    init() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "memoryWarningReceived", name: UIApplicationDidReceiveMemoryWarningNotification, object: nil)
+    }
+    
+    func memoryWarningReceived() {
+        memoryCache.removeAll()
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+}
+
+extension Lily {
+    class DiskCache {
+        var cache = NSCache()
+    
+    }
+}
+
+
+extension Lily {
+    class QuickCache {
+        var cache = NSCache()
+        
+        // MARK:  Initialization
+        
+        init() {
+            NSNotificationCenter.defaultCenter().addObserver(self, selector: "cacheToDisk", name: UIApplicationDidEnterBackgroundNotification, object: nil)
+        }
+        
+        deinit {
+            NSNotificationCenter.defaultCenter().removeObserver(self)
+        }
+        
+        func cacheToDisk() {
+            for key in cache.allKeys {
+                if let object: AnyObject = cache[key] {
+                    let data = NSKeyedArchiver.archivedDataWithRootObject(object)
+                    FileManager.write(data, filename: key)
+                }
+            }
+        }
+        
+        // MARK: 
+        
+        subscript(key: String) -> CacheProxy {
+            return CacheProxy(key, cache)
+        }
+        
+        subscript(key: String) -> AnyObject? {
+            get {
+                return self[key]
+            }
+            set {
+                cache[key] = newValue
+            }
+        }
+    }
 }
--- Lily/NSCache+Ex.swift
+++ Lily/NSCache+Ex.swift
@@ -1,5 +1,5 @@
 //
-//  NSCacheExtension.swift
+//  NSCache+Ex.swift
 //  SwifyCache
 //
 //  Created by kukushi on 3/24/15.
@@ -8,57 +8,30 @@
 
 import Foundation
 
+private var NSCacheKeysKey: UInt8 = 0
 
 public extension NSCache {
-    // Inner proxy class
-    class CacheProxy {
-        private let key: String
-        private let cache: NSCache
-        
-        init (_ key: String, _ cache: NSCache) {
-            self.key = key
-            self.cache = cache
+    
+    var allKeys: Set<String> {
+        return _allKeys
+    }
+    
+    private var _allKeys: Set<String>! {
+        get {
+            var keys = objc_getAssociatedObject(self, &NSCacheKeysKey) as? Set<String>
+            if keys == nil {
+                keys = Set<String>()
+                self._allKeys = keys
+            }
+            return keys!
         }
-        
-        func remove() {
-            cache.removeObjectForKey(key)
+        set(newValue) {
+            objc_setAssociatedObject(self, &NSCacheKeysKey, newValue, objc_AssociationPolicy(OBJC_ASSOCIATION_RETAIN))
         }
-        
-        var object: AnyObject? {
-            return cache.objectForKey(key)
-        }
-        
-        var string: String {
-            return (object as? String) ?? ""
-        }
-        
-        var data: NSData {
-            return (object as? NSData) ?? NSData()
-        }
-        
-        var number: NSNumber? {
-            return object as? NSNumber
-        }
-        
-        var value: NSValue? {
-            return object as? NSValue
-        }
-        
-        var bool: Bool {
-            return (object as? Bool) ?? false
-        }
-        
-        var double: Double {
-            return number?.doubleValue ?? 0
-        }
-        
-        var float: Float {
-            return number?.floatValue ?? 0
-        }
-        
-        var int: Int {
-            return number?.integerValue ?? 0
-        }
+    }
+    
+    public func removeAll() {
+        removeAllObjects()
     }
     
     subscript(key: String) -> CacheProxy {
@@ -71,12 +44,60 @@ public extension NSCache {
         }
         set {
             if newValue != nil {
+                _allKeys.insert(key)
                 setObject(newValue!, forKey: key)
             }
         }
     }
+}
+
+public class CacheProxy {
+    private let key: String
+    private let cache: NSCache
     
-    public func removeAll() {
-        removeAllObjects()
+    init (_ key: String, _ cache: NSCache) {
+        self.key = key
+        self.cache = cache
+    }
+    
+    func remove() {
+        cache._allKeys.remove(key)
+        cache.removeObjectForKey(key)
+    }
+    
+    var object: AnyObject? {
+        return cache.objectForKey(key)
+    }
+    
+    var string: String {
+        return (object as? String) ?? ""
+    }
+    
+    var data: NSData {
+        return (object as? NSData) ?? NSData()
+    }
+    
+    var number: NSNumber? {
+        return object as? NSNumber
+    }
+    
+    var value: NSValue? {
+        return object as? NSValue
+    }
+    
+    var bool: Bool {
+        return (object as? Bool) ?? false
+    }
+    
+    var double: Double {
+        return number?.doubleValue ?? 0
+    }
+    
+    var float: Float {
+        return number?.floatValue ?? 0
+    }
+    
+    var int: Int {
+        return number?.integerValue ?? 0
     }
 }
